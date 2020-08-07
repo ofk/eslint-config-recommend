430ca5d1055e7195fbba21fe2749a3d26f9b0af9
ofk <ofkjpn+github@gmail.com>
2020-08-07 18:41:58 +0900

    Add prettier/@typescript-eslint to extends
    I forget to disable @typescript-eslint rules for prettier

--- prettier.js
+++ prettier.js
@@ -1,5 +1,9 @@
 const { isInstalled } = require('./utils');
 
 module.exports = {
-  extends: ['plugin:prettier/recommended', ...(isInstalled('react') ? ['prettier/react'] : [])],
+  extends: [
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+    ...(isInstalled('react') ? ['prettier/react'] : []),
+  ],
 };
