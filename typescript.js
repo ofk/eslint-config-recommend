b7e3a89287a295aed1291e643869cb729c63fad6
ofk <ofkjpn+github@gmail.com>
2021-06-24 00:21:36 +0900

    Refactoring typescript settings

--- typescript.js
+++ typescript.js
@@ -1,22 +1,16 @@
-const airbnbTypeScriptConfig = require('eslint-config-airbnb-typescript');
-const airbnbTypeScriptShared = require('eslint-config-airbnb-typescript/lib/shared');
-
 const { isInstalled } = require('./utils');
 
 module.exports = {
   overrides: [
     {
       files: ['**/*.ts', '**/*.tsx'],
-      extends: ['plugin:import/typescript', 'plugin:@typescript-eslint/recommended'],
-      settings: {
-        ...airbnbTypeScriptShared.settings,
-        ...(isInstalled('react') ? airbnbTypeScriptConfig.settings : {}),
-      },
-      rules: {
-        ...airbnbTypeScriptShared.rules,
-        ...airbnbTypeScriptShared.overrides[0].rules,
-        ...(isInstalled('react') ? airbnbTypeScriptConfig.rules : {}),
-      },
+      extends: [
+        isInstalled('react')
+          ? 'eslint-config-airbnb-typescript'
+          : 'eslint-config-airbnb-typescript/base',
+        'plugin:import/typescript',
+        'plugin:@typescript-eslint/recommended',
+      ],
     },
   ],
 };
