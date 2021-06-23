bf6c03b64004e2be1aefed277e9ad46c9d49f73d
ofk <ofkjpn+github@gmail.com>
2021-06-23 19:37:04 +0900

    Add URLs into comment

--- comments.js
+++ comments.js
@@ -1,12 +1,14 @@
 module.exports = {
   extends: ['plugin:eslint-comments/recommended'],
   rules: {
+    // https://mysticatea.github.io/eslint-plugin-eslint-comments/rules/disable-enable-pair.html
     'eslint-comments/disable-enable-pair': [
       'error',
       {
         allowWholeFile: true,
       },
     ],
+    // https://mysticatea.github.io/eslint-plugin-eslint-comments/rules/no-unused-disable.html
     'eslint-comments/no-unused-disable': 'error',
   },
 };
