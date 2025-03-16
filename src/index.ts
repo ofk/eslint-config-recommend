0e05bd72614f096b89a2651a90ae784e8cbec6cb
ofk <ofkjpn+github@gmail.com>
2025-03-16 18:17:34 +0900

    Add settings for eslint-import-resolver-typescript

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,16 @@ import { isInstalled } from './isInstalled';
 export default function defineConfig(
   options: Parameters<typeof config>[0] = {},
 ): ReturnType<typeof config> {
+  const settings = { ...options.settings };
+  if (isInstalled('eslint-import-resolver-typescript')) {
+    settings['import/resolver'] ??= {};
+    (settings['import/resolver'] as Record<string, unknown>).typescript = {};
+  }
   return config({
     ...options,
     extends: [isInstalled('prettier') ? configPrettier : {}, ...(options.extends ?? [])],
     react: 'react' in options ? options.react : isInstalled('react') ? {} : false,
+    settings,
     ts: 'ts' in options ? options.ts : isInstalled('typescript') ? {} : false,
     vitest: 'vitest' in options ? options.vitest : isInstalled('vitest') ? {} : false,
   });
